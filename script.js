089d9a7104689bd8c8c0bd2a26163a065285a5d0
Manideep_048 <100527457+Manideep048@users.noreply.github.com>
2024-02-05 12:50:41 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var videoContainer = document.querySelector('.video-container');
+    videoContainer.style.opacity = 1; // Fade in the video container
+});
