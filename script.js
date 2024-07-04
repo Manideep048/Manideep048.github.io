380f98b5e9522c0642e0467071be9ef243b99822
Manideep_048 <100527457+Manideep048@users.noreply.github.com>
2024-07-05 02:41:58 +0530

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var videoContainer = document.querySelector('.video-container');
-    videoContainer.style.opacity = 1; // Fade in the video container
-});
